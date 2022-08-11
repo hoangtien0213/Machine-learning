515627849b41dc53015f009f4f8d079d01c5997d
Duc Tran <tranduc2312@gmail.com>
2022-08-11 22:08:54 +0700

    Update convertCsvToModel

--- house-rent-prediction/src/HouseRentPredict.java
+++ house-rent-prediction/src/HouseRentPredict.java
@@ -1,32 +1,29 @@
-import java.util.Arrays;
+import java.util.ArrayList;
 import java.util.List;
 
-import rent.model.HouseCsvModel;
+import rent.model.HouseDataModel;
 import rent.model.PredictModel;
 import rent.util.CsvUtil;
 
 public class HouseRentPredict {
-	
-	private static CsvUtil csvUtil = new CsvUtil();
-	
-	public static void main(String[] args) {
 
-		// read data train
-		// List<HouseCsvModel> dataTrainList = 
-		csvUtil.readDataFromCsv("test.csv");
+    private static CsvUtil csvUtil = new CsvUtil();
 
-		// Train TODO
+    public static void main(String[] args) {
 
-		// read data input to predict
-		// List<HouseCsvModel> dataHouseList = csvUtil.readDataFromCsv("rent.csv");
-		
-		// Write predict
-		List<PredictModel> dataPredictList = Arrays.asList(
-			new PredictModel("1","1000"),
-			new PredictModel("2","2000"),
-			new PredictModel("3","3000")
-		);
-		csvUtil.writeDataToCsv(dataPredictList);
-	}
+        List<HouseDataModel> dataHouseList = csvUtil.readDataFromCsvInput();
+
+        List<HouseDataModel> dataTrainList = csvUtil.readDataFromCsvTraining();
+
+        List<PredictModel> dataPredictList = new ArrayList();
+        for (HouseDataModel data : dataTrainList) {
+            dataPredictList.add(new PredictModel(data.getId(), "" + data.getTimeToStation()));
+        }
+
+//         Write predict
+//        List<PredictModel> dataPredictList = Arrays.asList(new PredictModel("1", "1000"), new PredictModel("2", "2000"),
+//                new PredictModel("3", "3000"));
+        csvUtil.writeDataToCsv(dataPredictList);
+    }
 
 }
--- house-rent-prediction/src/rent/util/CsvUtil.java
+++ house-rent-prediction/src/rent/util/CsvUtil.java
@@ -20,183 +20,181 @@ import rent.model.HouseCsvModel;
 import rent.model.HouseDataModel;
 import rent.model.PredictModel;
 
-
-
 public class CsvUtil {
 
-	public List<HouseDataModel> readDataFromCsv(String fileName) {
-		List<HouseDataModel> dataHouseList = new ArrayList<HouseDataModel>();
-		Path path1 = Paths.get(fileName);
+    public List<HouseDataModel> readDataFromCsvInput() {
+        List<HouseDataModel> dataHouseList = new ArrayList<HouseDataModel>();
+        Path path1 = Paths.get("test.csv");
 
-		File file = new File(path1.toUri());
+        File file = new File(path1.toUri());
 
-		try {
-			InputStreamReader csvStreamReader = new InputStreamReader(new FileInputStream(file), "UTF-8");
-			CSVReader reader = new CSVReader(csvStreamReader);
-			// skip header
-			reader.skip(1);
-			String[] nextLine;
-			while ((nextLine = reader.readNext()) != null) {
-				// System.out.println(Arrays.toString(nextLine));
-				HouseCsvModel data = new HouseCsvModel(
-						nextLine[0],
-						nextLine[1],
-						nextLine[2],
-						nextLine[3],
-						nextLine[4],
-						nextLine[5],
-						nextLine[6],
-						nextLine[7],
-						nextLine[8],
-						nextLine[9],
-						nextLine[10],
-						nextLine[11],
-						nextLine[12],
-						nextLine[13],
-						nextLine[14],
-						nextLine[15],
-						nextLine[16],
-						nextLine[17],
-						nextLine[18],
-						nextLine[19],
-						nextLine[20],
-						nextLine[21],
-						nextLine[22],
-						nextLine[23],
-						nextLine[24],
-						nextLine[25],
-						nextLine[26]);
+        try {
+            InputStreamReader csvStreamReader = new InputStreamReader(new FileInputStream(file), "UTF-8");
+            CSVReader reader = new CSVReader(csvStreamReader);
+            // skip header
+            reader.skip(1);
+            String[] nextLine;
+            while ((nextLine = reader.readNext()) != null) {
+                // System.out.println(Arrays.toString(nextLine));
+                HouseCsvModel data = new HouseCsvModel(nextLine[0], nextLine[1], null, nextLine[2], nextLine[3], nextLine[4],
+                        nextLine[5], nextLine[6], nextLine[7], nextLine[8], nextLine[9], nextLine[10], nextLine[11],
+                        nextLine[12], nextLine[13], nextLine[14], nextLine[15], nextLine[16], nextLine[17],
+                        nextLine[18], nextLine[19], nextLine[20], nextLine[21], nextLine[22], nextLine[23],
+                        nextLine[24], nextLine[25]);
 
-				dataHouseList.add(this.convertCsvToModel(data));
-			}
-		} catch (FileNotFoundException e) {
-			e.printStackTrace();
-			System.out.println("Error");
-		} catch (Exception e) {
-			// TODO: handle exception
-			System.out.println(e);
-		}
-		return dataHouseList;
-	}
+                dataHouseList.add(this.convertCsvToModel(data));
+            }
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (Exception e) {
+            // TODO: handle exception
+            e.printStackTrace();
+        }
+        return dataHouseList;
+    }
 
-	public void writeDataToCsv(List<PredictModel> dataPredictList) {
-		File file = new File("predict.csv");
-		try (CSVWriter writer = new CSVWriter(new FileWriter(file))) {
-			// create file if not exists
-			if (!file.exists()) {
-				file.createNewFile();
-			}
-			// write integers
-			writer.writeNext(new String[] { "ID", "Cost" });
-			for (PredictModel predictModel : dataPredictList) {
-				writer.writeNext(predictModel.toArrayString());
-			}
-			System.out.println("File written successfully.");
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
-	}
+    public List<HouseDataModel> readDataFromCsvTraining() {
+        List<HouseDataModel> dataHouseList = new ArrayList<HouseDataModel>();
+        Path path1 = Paths.get("rent.csv");
 
-	public void writeDataToCsv(String data) {
-		File file = new File("predict.csv");
-		try (BufferedWriter writer = new BufferedWriter(new FileWriter(file))) {
-			// create file if not exists
-			if (!file.exists()) {
-				file.createNewFile();
-			}
-			// write integers
-			writer.write("head");
-			writer.newLine();
-			writer.write(data);
-			System.out.println("File written successfully.");
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
-	}
+        File file = new File(path1.toUri());
 
-	// Todo
-	private HouseDataModel convertCsvToModel(HouseCsvModel csv) {
-		HouseDataModel data = new HouseDataModel();
-		// ID
-		data.setId(csv.getId());
-		
-		// deposit
-		// keyMoney
-		
-		// Address
-		data.setAddress(1);
-		// Time to station
-		final String regex = "\\d+";
-		final Pattern pattern = Pattern.compile(regex, Pattern.MULTILINE);
-		final Matcher matcher = pattern.matcher(csv.getTimeToStation());
-		long min = 0;
-		int count = 0;
-		while (matcher.find()) {
-			min += Integer.parseInt(matcher.group(0));
-			count++;
-		}
-		data.setStationCount(count);
-		data.setTimeToStation(min / count);
+        try {
+            InputStreamReader csvStreamReader = new InputStreamReader(new FileInputStream(file), "UTF-8");
+            CSVReader reader = new CSVReader(csvStreamReader);
+            // skip header
+            reader.skip(1);
+            String[] nextLine;
+            while ((nextLine = reader.readNext()) != null) {
+                // System.out.println(Arrays.toString(nextLine));
+                HouseCsvModel data = new HouseCsvModel(nextLine[0], nextLine[1], nextLine[2], nextLine[3], nextLine[4],
+                        nextLine[5], nextLine[6], nextLine[7], nextLine[8], nextLine[9], nextLine[10], nextLine[11],
+                        nextLine[12], nextLine[13], nextLine[14], nextLine[15], nextLine[16], nextLine[17],
+                        nextLine[18], nextLine[19], nextLine[20], nextLine[21], nextLine[22], nextLine[23],
+                        nextLine[24], nextLine[25], nextLine[26]);
 
-		// StructureAndDesign
-		// https://resources.realestate.co.jp/living/1r-1k-1dk-1ldk-apartment-whats-the-difference-and-which-should-i-rent/
-		// https://resources.realestate.co.jp/living/what-is-a-2ldk-apartment-real-estate-japans-word-of-the-day/
-		if ("ワンルーム".equals(csv.getStructureAndDesign())) {
-			data.setStructureAndDesign(1);
-		} 
-		else {
-			double roomNum = Double.parseDouble(csv.getStructureAndDesign().substring(0,1));
-			double subRoom = (csv.getStructureAndDesign().length() - 1) / 2.0;
-			data.setStructureAndDesign(roomNum + subRoom);
-		}
-		
-		// totalAsableArea
-		data.setTotalUsableArea(Double.parseDouble(csv.getTotalUsableArea().substring(0, csv.getTotalUsableArea().length() - 2)));
+                dataHouseList.add(this.convertCsvToModel(data));
+            }
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (Exception e) {
+            // TODO: handle exception
+            e.printStackTrace();
+        }
+        return dataHouseList;
+    }
+    
+    public void writeDataToCsv(List<PredictModel> dataPredictList) {
+        File file = new File("predict.csv");
+        try (CSVWriter writer = new CSVWriter(new FileWriter(file))) {
+            // create file if not exists
+            if (!file.exists()) {
+                file.createNewFile();
+            }
+            // write integers
+            writer.writeNext(new String[] { "ID", "Cost" });
+            for (PredictModel predictModel : dataPredictList) {
+                writer.writeNext(predictModel.toArrayString());
+            }
+            System.out.println("File written successfully.");
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
 
-		// numberOfYearsSinceConstruction
-		if ("新築".equals(csv.getNumberOfYearsSinceConstruction())) {
-		    data.setNumberOfYearsSinceConstruction(1);
-		} else {
-		    data.setNumberOfYearsSinceConstruction(
-		            Double.parseDouble(csv.getNumberOfYearsSinceConstruction().replaceAll("\\D","")));
-		}
-		
-		// floor
-		if ("-".equals(csv.getFloor())) {
-		    data.setFloor(1);
-		} else {
-		    data.setFloor(Double.parseDouble(csv.getFloor().substring(0, 1)));
-		}
-		
-		// direction TODO
-		// kindsOfHouse TODO
-		// featuresAndEquipment  
-	    data.setFeaturesAndEquipment(csv.getFeaturesAndEquipment().split("、").length);
-		
-		// texture TODO
-		// totalFloor TODO
-		// parkingLot TODO
+    public void writeDataToCsv(String data) {
+        File file = new File("predict.csv");
+        try (BufferedWriter writer = new BufferedWriter(new FileWriter(file))) {
+            // create file if not exists
+            if (!file.exists()) {
+                file.createNewFile();
+            }
+            // write integers
+            writer.write("head");
+            writer.newLine();
+            writer.write(data);
+            System.out.println("File written successfully.");
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
 
-        System.out.println(csv.getFeaturesAndEquipment());
-		System.out.println(data.getFeaturesAndEquipment());
-		return data;
-	}
+    // Todo
+    private HouseDataModel convertCsvToModel(HouseCsvModel csv) {
+        HouseDataModel data = new HouseDataModel();
+        // ID
+        data.setId(csv.getId());
 
-	public static void main(String[] args) {
-		final String regex = "\\d+";
-		final String string = "西武新宿線/都立家政駅 歩2分\n"
-				+ "西武新宿線/鷺ノ宮駅 歩9分\n"
-				+ "西武池袋線/中村橋駅 歩27分";
+        // Address
+        // data.setAddress(1);
 
-		final Pattern pattern = Pattern.compile(regex, Pattern.MULTILINE);
-		final Matcher matcher = pattern.matcher(string);
+        // Time to station
+        final String regex = "\\d+";
+        final Pattern pattern = Pattern.compile(regex, Pattern.MULTILINE);
+        final Matcher matcher = pattern.matcher(csv.getTimeToStation());
+        long min = 0;
+        int count = 0;
+        while (matcher.find()) {
+            // min += Integer.parseInt(matcher.group(0));
+            if (count == 0) {
+                min = Integer.parseInt(matcher.group(0));
+            } else {
+                long temp = Integer.parseInt(matcher.group(0));
+                if (temp < min) {
+                    min = temp;
+                }
+            }
+            count++;
+        }
+        data.setStationCount(count);
+        data.setTimeToStation(min);
 
-		while (matcher.find()) {
-			System.out.println("Full match: " + matcher.group(0));
+        // StructureAndDesign
+        // https://resources.realestate.co.jp/living/1r-1k-1dk-1ldk-apartment-whats-the-difference-and-which-should-i-rent/
+        // https://resources.realestate.co.jp/living/what-is-a-2ldk-apartment-real-estate-japans-word-of-the-day/
+        if ("ワンルーム".equals(csv.getStructureAndDesign())) {
+            data.setStructureAndDesign(1);
+        } else {
+            double roomNum = Double.parseDouble(csv.getStructureAndDesign().substring(0, 1));
+            double subRoom = (csv.getStructureAndDesign().length() - 1) / 2.0;
+            data.setStructureAndDesign(roomNum + subRoom);
+        }
 
-			for (int i = 1; i <= matcher.groupCount(); i++) {
-				System.out.println("Group " + i + ": " + matcher.group(i));
-			}
-		}
-	}
+        // totalAsableArea
+        data.setTotalUsableArea(
+                Double.parseDouble(csv.getTotalUsableArea().substring(0, csv.getTotalUsableArea().length() - 2)));
+
+        // numberOfYearsSinceConstruction
+        if ("新築".equals(csv.getNumberOfYearsSinceConstruction())) {
+            data.setNumberOfYearsSinceConstruction(1);
+        } else {
+            data.setNumberOfYearsSinceConstruction(
+                    Double.parseDouble(csv.getNumberOfYearsSinceConstruction().replaceAll("\\D", "")));
+        }
+
+        // floor
+        if ("-".equals(csv.getFloor())) {
+            data.setFloor(1);
+        } else {
+            data.setFloor(Double.parseDouble(csv.getFloor().substring(0, 1)));
+        }
+
+        // kindsOfHouse TODO
+        if ("マンション".equals(csv.getKindsOfHouse())) {
+            data.setKindsOfHouse(5);
+        } else if ("アパート".equals(csv.getKindsOfHouse())) {
+            data.setKindsOfHouse(4);
+        } else if ("一戸建て".equals(csv.getKindsOfHouse())) {
+            data.setKindsOfHouse(3);
+        } else if ("テラス・タウンハウス".equals(csv.getKindsOfHouse())) {
+            data.setKindsOfHouse(2);
+        } else {
+            data.setKindsOfHouse(1);
+        }
+
+        // featuresAndEquipment
+        data.setFeaturesAndEquipment(csv.getFeaturesAndEquipment().split("、").length);
+
+        return data;
+    }
 }
